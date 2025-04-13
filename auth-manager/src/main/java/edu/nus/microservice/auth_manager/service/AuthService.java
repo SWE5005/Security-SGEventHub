f67179afcad72d3befca1a695e4063e186e7004f
Kaidi Zhang <teric@qq.com>
2025-04-13 17:00:38 +0800

    [SUS-11] feat: add refresh token

--- auth-manager/src/main/java/edu/nus/microservice/auth_manager/service/AuthService.java
+++ auth-manager/src/main/java/edu/nus/microservice/auth_manager/service/AuthService.java
@@ -2,9 +2,10 @@ package edu.nus.microservice.auth_manager.service;
 
 import edu.nus.microservice.auth_manager.dto.AuthResponse;
 import edu.nus.microservice.auth_manager.dto.UserRegistrationRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 
 public interface AuthService {
-    AuthResponse getJwtTokensAfterAuthentication(Authentication authentication);
+    AuthResponse getJwtTokensAfterAuthentication(Authentication authentication, HttpServletResponse response);
     AuthResponse registerUser(UserRegistrationRequest userRegistrationRequest);
 }
