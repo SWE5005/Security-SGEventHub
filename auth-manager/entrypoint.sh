a60052efbed3104a42244dc624706b9349b7fe5a
FangZiyue <E0838388@u.nus.edu>
2025-04-30 14:16:47 +0800

    fix:backend-auto-deployment-change
    modify docker file
    modify properties into env var includes:
    db connection parameter
    public key & private key
    client id & secret

---
+++ auth-manager/entrypoint.sh
@@ -0,0 +1,8 @@
+mkdir -p /app/certs
+
+# Decode base64 certs
+echo "$privateKey_B64" | base64 -d > /app/certs/privateKey.pem
+echo "$publicKey_B64" | base64 -d > /app/certs/publicKey.pem
+
+# Launch Spring Boot
+exec java -jar auth-manager.jar
