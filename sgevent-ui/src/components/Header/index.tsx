577007f361628f0f613fcbc90fc59652755cfda1
Dois <kohehd@gmail.com>
2025-04-30 21:50:50 +0800

    feat: added nav items to headerbar for event and and user based on permissions

--- sgevent-ui/src/components/Header/index.tsx
+++ sgevent-ui/src/components/Header/index.tsx
@@ -2,31 +2,18 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { selectUserName } from '../../state/auth/slice';
-import { useSelector } from 'react-redux';
-import { navigate } from 'gatsby';
+import { navigate, Link } from 'gatsby';
 import { useLogoutMutation } from '../../services/auth.service';
+import Button from '@mui/material/Button';
+import Permission from '../Permission';
 
 export default function Header() {
-  const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
   const [requestLogout, result] = useLogoutMutation();
-  const userName = useSelector(selectUserName); // get the username directly
 
   const handleNavigateHome = () => {
-    navigate('/login'); // double check this route
-  };
-  const handleMenu: React.MouseEventHandler<HTMLAnchorElement> = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+    navigate('/');
   };
 
   const performLogout = () => {
@@ -35,7 +22,6 @@ export default function Header() {
 
   React.useEffect(() => {
     if (result.isSuccess) {
-      // Redirect to the login page
       navigate('/login');
     }
   }, [result]);
@@ -44,51 +30,31 @@ export default function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="go to home" sx={{ mr: 2 }} onClick={handleNavigateHome}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="go to home"
+            sx={{ mr: 2 }}
+            onClick={handleNavigateHome}
+          >
             <HomeIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            SG EventHub
-          </Typography>
-
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>{userName ?? 'Profile'}</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                  performLogout();
-                }}
-              >
-                Log out
-              </MenuItem>
-            </Menu>
-          </div>
+          <Box sx={{ display: 'flex', flexGrow: 1 }}>
+            <Permission authKeyList={['SUPER_ADMIN']} isRedirectOnFail={false}>
+              <Button color="inherit" component={Link} to="/users">
+                Users
+              </Button>
+            </Permission>
+            <Permission authKeyList={['SUPER_ADMIN', 'EVENT_MANAGER', 'END_USER']} isRedirectOnFail={false}>
+              <Button color="inherit" component={Link} to="/events">
+                Events
+              </Button>
+            </Permission>
+          </Box>
+          <Button color="inherit" onClick={performLogout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
